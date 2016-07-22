d4c13c1a7bc5247e38b139fc761a53096bfdd1a9
mewmew <rnd0x00@gmail.com>
2016-07-22 04:09:40 +0200

    metautil: Add AppendBlockBody.

--- metautil.go
+++ metautil.go
@@ -3,12 +3,46 @@
 package metautil
 
 import (
+	"log"
+
 	"github.com/mewkiz/flac"
 	"github.com/mewkiz/flac/meta"
 )
 
 // AppendBlock appends the given metadata block to the FLAC stream.
 func AppendBlock(stream *flac.Stream, block *meta.Block) {
+	stream.Blocks = append(stream.Blocks, block)
+}
+
+// AppendBlockBody appends the given metadata block body to the FLAC stream.
+func AppendBlockBody(stream *flac.Stream, body interface{}) {
+	// NOTE: The block header omit values for IsLast and Length. These are added
+	// by flac.Encode when encoding the FLAC stream. Whether this is a good
+	// approach or not is yet to be decided.
+	block := new(meta.Block)
+	switch body := body.(type) {
+	case *meta.StreamInfo:
+		block.Type = meta.TypeStreamInfo
+		block.Body = body
+	case *meta.Application:
+		block.Type = meta.TypeApplication
+		block.Body = body
+	case *meta.SeekTable:
+		block.Type = meta.TypeSeekTable
+		block.Body = body
+	case *meta.VorbisComment:
+		block.Type = meta.TypeVorbisComment
+		block.Body = body
+	case *meta.CueSheet:
+		block.Type = meta.TypeCueSheet
+		block.Body = body
+	case *meta.Picture:
+		block.Type = meta.TypePicture
+		block.Body = body
+	default:
+		log.Printf("support for appending metadata block body of type %T not yet implemented", body)
+		return
+	}
 	stream.Blocks = append(stream.Blocks, block)
 }
 
@@ -38,13 +72,13 @@ func RemoveBlockType(stream *flac.Stream, typ meta.Type) {
 
 // A CommentBlock represents a Vorbis comment metadata block.
 type CommentBlock struct {
-	Block *meta.VorbisComment
+	Body *meta.VorbisComment
 }
 
 // NewCommentBlock returns a new Vorbis comment metadata block.
 func NewCommentBlock(vendor string) *CommentBlock {
 	return &CommentBlock{
-		Block: &meta.VorbisComment{
+		Body: &meta.VorbisComment{
 			Vendor: vendor,
 		},
 	}
@@ -53,5 +87,5 @@ func NewCommentBlock(vendor string) *CommentBlock {
 // Add adds the given key-value pair to the Vorbis comment metadata block.
 func (c *CommentBlock) Add(key, val string) {
 	tag := [2]string{key, val}
-	c.Block.Tags = append(c.Block.Tags, tag)
+	c.Body.Tags = append(c.Body.Tags, tag)
 }
